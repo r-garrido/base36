a50b917b409524f456f19c2d56b271c058c591c9
Martin Lindhe <martin-commit@ubique.se>
2016-04-28 14:57:12 +0200

    cosmetics

--- base36_test.go
+++ base36_test.go
@@ -9,14 +9,12 @@ import (
 
 func TestEncode(t *testing.T) {
 
-	assert.Equal(t, "15N9Z8L3AU4EB", Encode(5481594952936519619))
-
 	assert.Equal(t, "1Y2P0IJ32E8E7", Encode(math.MaxInt64))
+	assert.Equal(t, "15N9Z8L3AU4EB", Encode(5481594952936519619))
 }
 
 func TestDecode(t *testing.T) {
 
-	assert.Equal(t, int64(5481594952936519619), Decode("15N9Z8L3AU4EB"))
-
 	assert.Equal(t, int64(math.MaxInt64), Decode("1Y2P0IJ32E8E7"))
+	assert.Equal(t, int64(5481594952936519619), Decode("15N9Z8L3AU4EB"))
 }
